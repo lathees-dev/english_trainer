84976fe2f52f5811250f839049a186622b7e30e0
Lathees <latheesraja655@gmail.com>
2025-01-02 14:49:04 +0530

    feat: added login and signup

--- trainer/urls.py
+++ trainer/urls.py
@@ -2,26 +2,40 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),  # Home page
-    path('grammar-options/', views.grammar_options, name='grammar_options'),  # Grammar Options
-    path('vocab-options/', views.vocab_options, name='vocab_options'),  # Vocabulary Options
-    path('vocab_learn/', views.vocab_learn, name='vocab_learn'),  # Vocabulary Learn
-    path('vocabulary/', views.vocabulary, name='vocabulary'),  # Vocabulary
-    path('exercise-options/<str:exercise_type>/', views.exercise_options, name='exercise_options'),
-    path('preposition/', views.preposition, name='preposition'),  # Prepositions
-    path('articles/', views.articles, name='articles'),
-    path('sentence_formation/', views.sentence_formation, name='sentence_formation'),
-    path('active_passive/', views.active_passive, name='active_passive'),
-    path('direct_indirect/', views.direct_indirect, name='direct_indirect'),
-    path('learn/<str:exercise_type>/', views.learn_exercise, name='learn_exercise'),
-    path('fillup/<str:question_type>/', views.fillup, name='fillup'),
-    path('speaking/', views.speaking, name='speaking'),
-    path('generate_speaking_statement/', views.generate_speaking_statement, name='generate_speaking_statement'),
-    path('conjunctions/', views.conjunctions, name='conjunctions'),
-    path('interjections/', views.interjections, name='interjections'),
-    path('nouns/', views.nouns, name='nouns'),
-    path('pronouns/', views.pronouns, name='pronouns'),
-    path('tenses/', views.tenses, name='tenses'),
-    path('verbs_adverbs/', views.verbs_adverbs, name='verbs_adverbs'),
-    path('adjectives/', views.adjectives, name='adjectives'),
-    ]
+    path("", views.login, name="login"),
+    path("home/", views.home, name="home"),  # Home page
+    path("signup/", views.signup, name="signup"),
+    path(
+        "grammar-options/", views.grammar_options, name="grammar_options"
+    ),  # Grammar Options
+    path(
+        "vocab-options/", views.vocab_options, name="vocab_options"
+    ),  # Vocabulary Options
+    path("vocab_learn/", views.vocab_learn, name="vocab_learn"),  # Vocabulary Learn
+    path("vocabulary/", views.vocabulary, name="vocabulary"),  # Vocabulary
+    path(
+        "exercise-options/<str:exercise_type>/",
+        views.exercise_options,
+        name="exercise_options",
+    ),
+    path("preposition/", views.preposition, name="preposition"),  # Prepositions
+    path("articles/", views.articles, name="articles"),
+    path("sentence_formation/", views.sentence_formation, name="sentence_formation"),
+    path("active_passive/", views.active_passive, name="active_passive"),
+    path("direct_indirect/", views.direct_indirect, name="direct_indirect"),
+    path("learn/<str:exercise_type>/", views.learn_exercise, name="learn_exercise"),
+    path("fillup/<str:question_type>/", views.fillup, name="fillup"),
+    path("speaking/", views.speaking, name="speaking"),
+    path(
+        "generate_speaking_statement/",
+        views.generate_speaking_statement,
+        name="generate_speaking_statement",
+    ),
+    path("conjunctions/", views.conjunctions, name="conjunctions"),
+    path("interjections/", views.interjections, name="interjections"),
+    path("nouns/", views.nouns, name="nouns"),
+    path("pronouns/", views.pronouns, name="pronouns"),
+    path("tenses/", views.tenses, name="tenses"),
+    path("verbs_adverbs/", views.verbs_adverbs, name="verbs_adverbs"),
+    path("adjectives/", views.adjectives, name="adjectives"),
+]
